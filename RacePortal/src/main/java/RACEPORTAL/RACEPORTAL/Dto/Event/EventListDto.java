7a01ee3ce0e93b99ca7004522fd03bfd711807e1
Miłosz <milo.parkitny@gmail.com>
2025-12-13 15:28:17 +0100

    Projekt sie odpala, sprawdzilem kilka endpointów i sie dzialaja. Swagger tylko nie dziala ale nie jest on tak istotny mozemy w postmanie testowac ;)

--- RacePortal/src/main/java/RACEPORTAL/RACEPORTAL/Dto/Event/EventListDto.java
+++ RacePortal/src/main/java/RACEPORTAL/RACEPORTAL/Dto/Event/EventListDto.java
@@ -1,17 +1,21 @@
 package RACEPORTAL.RACEPORTAL.Dto.Event;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class EventListDto {
 
     private Long id;
     private String title;
+
     private LocalDate dateStart;
     private LocalDate dateEnd;
 
     private String categoryName;
     private String locationName;
     private String regionName;
-
-    // getters & setters
 }
