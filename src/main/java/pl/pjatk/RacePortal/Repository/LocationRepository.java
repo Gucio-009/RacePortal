5412013bb0f389a1c327f7242df1bb66e1ce13d7
Michał Gutowski <mgutowski2003@gmail.com>
2026-04-07 18:57:13 +0200

    Cleanning: clean, fresh, new start

--- src/main/java/pl/pjatk/RacePortal/Repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.pjatk.RacePortal.Repository;
-
-import  pl.pjatk.RacePortal.Entities.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
--- src/main/java/pl/pjatk/RacePortal/Repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.pjatk.RacePortal.Repository;
-
-import pl.pjatk.RacePortal.Entities.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
